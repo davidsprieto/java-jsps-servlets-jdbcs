b45690c0a5dd39cbb3d65c653bdd2a8bf80bf082
David Prieto <david.s.prieto1@gmail.com>
2022-08-11 11:45:45 -0500

    Completed Section 3 - #17

---
+++ src/main/java/com/codeup/udemyjspsservletsjdbcs/TestServlet.java
@@ -0,0 +1,17 @@
+package com.codeup.udemyjspsservletsjdbcs;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "testServlet", value = "/test")
+public class TestServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/test.jsp").forward(request, response);
+    }
+
+}
---
+++ target/udemy-jsps-servlets-jdbcs-1.0-SNAPSHOT/test.jsp
@@ -0,0 +1,28 @@
+<%@ page import="java.io.PrintWriter" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>JSPs, Servlets, JDBCs</title>
+</head>
+<body>
+
+<br/>
+<%-- EXPRESSIONS --%>
+Converting a string to uppercase: <%= new String("Hello World").toUpperCase() %>
+<br/>
+25 multiplied by 4 equals: <%= 25*4 %>
+<br/>
+Is 75 less than 69? <%= 75 < 69 %>
+<br>
+
+<%-- SCRIPLET --%>
+<%
+    PrintWriter outPrint = response.getWriter();
+
+    for (int i = 1; i <= 5; i++) {
+        outPrint.println("<br/> I really love to code: " + i);
+    }
+%>
+
+</body>
+</html>
